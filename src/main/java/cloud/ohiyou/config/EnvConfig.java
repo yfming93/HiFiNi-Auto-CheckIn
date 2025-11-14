f020276a9824b1cf0459e0e3e2c417947a3a1e6f
mingo <yfmingo@163.com>
2025-11-14 17:17:42 +0800

    nil

--- src/main/java/cloud/ohiyou/config/EnvConfig.java
+++ src/main/java/cloud/ohiyou/config/EnvConfig.java
@@ -15,8 +15,8 @@ public class EnvConfig {
 
 
     private EnvConfig() {
-        cookie = "bbs_sid=8o6rk2oagb88abk9g8jrgb4mf0; Hm_lvt_23819a3dd53d3be5031ca942c6cbaf25=1762436911; HMACCOUNT=25F9049004D86C0D; bbs_token=si0yaWEunuGlFnGo01sIMZw1Wzm1FJmGgp9KcDcvoaF_2B0im9srNmkwrH6NJC5Sa4GNZH4Qa88w4nI9q6NkzBRZb_2BbFs_3D; Hm_lpvt_23819a3dd53d3be5031ca942c6cbaf25=1762477668";
-        serverChan = "SCT301782T5pl5qKqGpBVSIpceSCjBQt47";
+        cookie = System.getenv("COOKIE");
+        serverChan = System.getenv("SERVER_CHAN");
         wxworkrobotkey = System.getenv("WXWORK_WEBHOOK");
         wxWorkRobotMessageType = System.getenv().getOrDefault("WXWORK_MSG_TYPE", "markdown");
         dingTalkRobotKey = System.getenv("DINGTALK_WEBHOOK");
